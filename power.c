333b84c908dfff5fafd6f19407efa49eadd7bcb6
snekavsb <31505096+snekavsb@users.noreply.github.com>
2018-01-19 16:04:49 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+# include <math.h>
+
+int main(void) {
+	int a,n,i;
+	scanf("%d\n",&a);
+	printf("%d\n",a);
+	scanf("%d\n",&n);
+	printf("%d\n",n);
+	i=pow(a,n);
+	printf("%d\n",i);
+	
+	return 0;
+}
