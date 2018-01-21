3e07f1c08a0e1cf5d3e481038e7b721cad3fb3d9
snekavsb <31505096+snekavsb@users.noreply.github.com>
2018-01-21 14:54:11 +0530

    Create ai.c

---
+++ ai.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void) {
+int r,sum=0,i,n,t,a;
+scanf("%d\n",&n);
+scanf("%d",&a);
+for(i=a+1;i<=n;i++)
+{
+{
+i=t;
+
+}
+
+
+while(t!=0)
+{
+	r=t%10;
+	sum=sum+r*r*r;
+
+	t=t/10;
+	
+}
+if(i==sum)
+{
+
+printf("%d\n",sum);
+}
+}
+
+return 0;
+}
