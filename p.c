e5944317dbd037cd8fb405f7000909b23cfc12a9
snekavsb <31505096+snekavsb@users.noreply.github.com>
2018-01-21 14:26:13 +0530

    Create p.c

---
+++ p.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(void) {
+int n,i,a,t=10;
+scanf("%d\n",&n);
+printf("%d\n",n);
+for(a=1;a<=t;a++)
+{
+	
+
+
+for(i=2;i<=n-1;i++)
+{
+	if(n%i==0)
+	
+{
+	printf("not a prime number\n");
+
+break;
+}
+}
+}
+
+if(i==n)
+
+
+{
+	printf(" a prime number");
+
+
+
+}
+
+	return 0;
+}
